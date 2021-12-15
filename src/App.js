feea642354dc9b71d50fe1856c6d9d1c4e64d7e4
uuganbayar.b <uuganbayar.b@nmma.co>
2021-12-15 18:15:06 +0800

    wrong words

--- src/App.js
+++ src/App.js
@@ -1,52 +1,55 @@
-import React, { useState } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import axios from 'axios';
 
 export default function App() {
-  const [value, setValue] = useState("<p>Peace?</p>");
-  const [text, setText] = useState("");
+  const [value, setValue] = useState('<p>Peace?</p>');
+  const [text, setText] = useState('');
+  const [lastSentence, setLastSentence] = useState([]);
 
   function onEditorChange(value, editor) {
     setValue(value);
-    setText(editor.getContent({ format: "text" }));
+    var words = value.split(' ');
+    console.log(words);
+
+    setText(editor.getContent({ format: 'text' }));
     axios
-      .get(
-        '/nlp-web-demo/process?text="' +
-          editor.getContent({ format: "text" }) +
-          '"'
-      )
+      .get('/nlp-web-demo/process?text="' + editor.getContent({ format: 'text' }) + '"')
       .then(function (response) {
         console.log(response);
+        setLastSentence(response.data.slice(-1)[0]);
       })
       .catch(function (error) {
         console.log(error);
       });
   }
 
+  const wrong = lastSentence.filter((word) => !word.lemma.includes('+'));
+  console.log('wrong words:', wrong);
   return (
     <>
       <Editor
-        onInit={(evt, editor) => setText(editor.getContent({ format: "text" }))}
+        onInit={(evt, editor) => setText(editor.getContent({ format: 'text' }))}
         onEditorChange={onEditorChange}
         initialValue="<p>Hello Editor</p>"
         init={{
           height: 500,
           menubar: false,
           plugins: [
-            "advlist autolink lists link image charmap print preview anchor",
-            "searchreplace visualblocks code fullscreen",
-            "insertdatetime media table paste code help wordcount",
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount',
           ],
           toolbar:
-            "undo redo | formatselect | " +
-            "bold italic backcolor | alignleft aligncenter " +
-            "alignright alignjustify | bullist numlist outdent indent | " +
-            "removeformat | help",
-          content_style:
-            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+            'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help' +
+            ' | spellchecker',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
         }}
-      />{" "}
-      {text}{" "}
+      />{' '}
+      {text}{' '}
     </>
   );
 }
